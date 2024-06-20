610455a925b2639ef157ae33910d63d8fb837ef0
srhsimon <sarah.simonca@gmail.com>
2024-06-20 10:31:50 -0400

    -rm line17

--- conf.py
+++ conf.py
@@ -13,9 +13,8 @@ author = 'sarah simon'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_rtd_theme'
-]
+extensions = []
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
